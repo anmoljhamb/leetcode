c419240440334f6a0b67c7d02d9f73f7e613b487
Anmol Jhamb <talktoanmol@outlook.com>
2023-01-18 13:41:00 +0530

    formatted the code

--- 009_palindrome.cpp
+++ 009_palindrome.cpp
@@ -1,23 +1,29 @@
 /**
-* * Question Link: https://leetcode.com/problems/palindrome-number/
-* * Sample Input:  x = 121
-* * Sample Output: true
-*/
+ * * Question Link: https://leetcode.com/problems/palindrome-number/
+ * * Sample Input:  x = 121
+ * * Sample Output: true
+ */
 
-class Solution {
+class Solution
+{
 public:
-    bool isPalindrome(int x) {
-        if ( x < 0 ) {
+    bool isPalindrome(int x)
+    {
+        if (x < 0)
+        {
             return false;
         }
         string number = "";
-        while ( x > 0 ){
-            number += (char)(x%10);
+        while (x > 0)
+        {
+            number += (char)(x % 10);
             x /= 10;
         }
         int n = number.length();
-        for ( int i=0; i<n/2; i++ ){
-            if ( number[i] != number[n-i-1] ){
+        for (int i = 0; i < n / 2; i++)
+        {
+            if (number[i] != number[n - i - 1])
+            {
                 return false;
             }
         }
