22bc289123ab5efd77da8e87ace2f12c27d681d7
Anmol Jhamb <talktoanmol@outlook.com>
2023-01-23 18:20:15 +0530

    poo5 working solution

--- 005_longest_palindrome_substr.py
+++ 005_longest_palindrome_substr.py
@@ -5,17 +5,32 @@ Problem Link: https://leetcode.com/problems/longest-palindromic-substring/
 
 class Solution:
 
-    def isPalindrome(self, s: str) -> bool:
+    def is_palindrome(self, s: str) -> bool:
         return s == s[::-1]
 
     def longestPalindrome(self, s: str) -> str:
-        max_len = 0
-        max_string = None
-        for i in range(len(s)):
-            curr_substr = ""
-            for j in range(i, len(s)):
-                curr_substr += s[j]
-                if (self.isPalindrome(curr_substr) and len(curr_substr) > max_len):
-                    max_len = len(curr_substr)
-                    max_string = curr_substr
-        return max_string
+
+        if (self.is_palindrome(s)):
+            return s
+
+        # Loop through the entire stirng.
+        # find the next occurnces. Check if they are a palindrome.
+
+        n = len(s)
+        max_sub = ""
+        for i in range(n):
+
+            current_char = s[i]
+            next_occurnces = [j for j in range(
+                i+1, n) if s[j] == current_char]
+
+            for j in next_occurnces:
+                curr_substr = s[i: j+1]
+                if (self.is_palindrome(curr_substr)):
+                    if (len(curr_substr) > len(max_sub)):
+                        max_sub = curr_substr
+
+        if len(max_sub) == 0:
+            return s[0]
+
+        return max_sub
