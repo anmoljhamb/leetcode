1aae59c80b8c1f22898fd40914d3a1de9051e70d
Anmol Jhamb <talktoanmol@outlook.com>
2023-07-15 00:14:25 +0530

    0(n) algorithm added in problem 001

--- 001_two_sums.ts
+++ 001_two_sums.ts
@@ -1,10 +1,28 @@
+interface Dict {
+    [num: number]: number[];
+}
+
 function twoSum(nums: number[], target: number): number[] {
+    const dict: Dict = {};
+    let result: number[] = [];
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (target === nums[i] + nums[j]) {
-                return [i, j];
+        let temp = nums[i];
+        if (!(temp in dict)) {
+            dict[temp] = [i];
+        } else dict[temp].push(i);
+    }
+
+    Object.keys(dict).forEach((value) => {
+        const x = Number.parseInt(value);
+        const y = target - x;
+        if (y in dict) {
+            if (x === y) {
+                result = [dict[x][0], dict[x][1]];
+            } else {
+                result = [dict[x][0], dict[y][0]];
             }
         }
-    }
-    return [];
+    });
+
+    return result;
 }
