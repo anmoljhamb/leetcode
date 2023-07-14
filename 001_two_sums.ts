4433d9e82cf11b19cf57bc4406b41f7a64e6c229
Anmol Jhamb <talktoanmol@outlook.com>
2023-07-15 00:21:41 +0530

    modify the approach to a single hash table search

--- 001_two_sums.ts
+++ 001_two_sums.ts
@@ -1,28 +1,20 @@
 interface Dict {
-    [num: number]: number[];
+    [num: number]: number;
 }
 
 function twoSum(nums: number[], target: number): number[] {
     const dict: Dict = {};
     let result: number[] = [];
     for (let i = 0; i < nums.length; i++) {
-        let temp = nums[i];
-        if (!(temp in dict)) {
-            dict[temp] = [i];
-        } else dict[temp].push(i);
-    }
-
-    Object.keys(dict).forEach((value) => {
-        const x = Number.parseInt(value);
-        const y = target - x;
+        let x = nums[i];
+        let y = target - x;
         if (y in dict) {
-            if (x === y) {
-                result = [dict[x][0], dict[x][1]];
-            } else {
-                result = [dict[x][0], dict[y][0]];
-            }
+            return [dict[y], i];
         }
-    });
+        if (!(x in dict)) {
+            dict[x] = i;
+        }
+    }
 
     return result;
 }
