17f638e0bae0aa9ebde7923903f700f8c0d68453
Anmol Jhamb <talktoanmol@outlook.com>
2023-08-15 10:42:21 +0530

    q26 - python - naive approach

--- 026_remove_duplicates.py
+++ 026_remove_duplicates.py
@@ -1,6 +1,12 @@
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
-        temp = set(nums)
+        temp = []
+
+        for (index, num) in enumerate(nums):
+            if (num) not in temp:
+                temp.append(num)
+
         for (index, num) in enumerate(temp):
             nums[index] = num
+
         return len(temp)
