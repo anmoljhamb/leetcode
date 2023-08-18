054f4b32bc600fec2451059038b748aad85bf360
Anmol Jhamb <talktoanmol@outlook.com>
2023-08-18 11:02:13 +0530

    hard question done

--- 042_trapping_water.ts
+++ 042_trapping_water.ts
@@ -2,27 +2,38 @@ function util(arr: number[], low: number, high: number) {
     let sum = 0;
     const k = Math.min(arr[low], arr[high]);
     for (let i = low + 1; i < high; i++) {
-        sum += k - arr[i];
+        sum += Math.max(k - arr[i], 0);
     }
     return sum;
 }
 
 function trap(arr: number[]): number {
-    let i = 0;
     let ans = 0;
-    while (i < arr.length - 1) {
-        // console.log(`Finding the next index for ${i}`);
-        let j = i + 1;
-        // Find the first j st arr[j] >= arr[i]
-        while (j < arr.length && arr[j] < arr[i]) j++;
-        if (j !== arr.length) {
-            ans += util(arr, i, j);
-            // console.log(`(${i}, ${j})`);
-            i = j;
-        } else {
-            i++;
-        }
+    const left = [...arr];
+    const right = [...arr];
+
+    for (let i = 1; i < arr.length; i++) {
+        left[i] = Math.max(left[i], left[i - 1]);
     }
+
+    for (let i = arr.length - 2; i >= 0; i--) {
+        right[i] = Math.max(right[i], right[i + 1]);
+    }
+
+    // console.log(arr);
+    // console.log(left);
+    // console.log(right);
+    left[0] = 0;
+    right[right.length - 1] = 0;
+
+    arr.forEach((height, index) => {
+        let temp = Math.min(left[index], right[index]) - height;
+        if (temp < 0) {
+            temp = 0;
+        }
+        ans += temp;
+    });
+
     return ans;
 }
 
--- driver.ts
+++ driver.ts
@@ -1,13 +1,17 @@
 import solution from "./042_trapping_water";
 
-// const input1 = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
-// const output1 = solution(input1);
-// const expected1 = 6;
-
-const input1 = [10, 3, 6, 7];
+const input1 = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
 const output1 = solution(input1);
 const expected1 = 6;
 
-console.log(`input1: ${input1}`);
+// console.log(`input1: ${input1}`);
 console.log(`output1: ${output1}`);
 console.log(`expectedOutput1: ${expected1}`);
+
+// const input2 = [10, 3, 6, 7];
+// const output2 = solution(input2);
+// const expected2 = 6;
+
+// // console.log(`input1: ${input2}`);
+// console.log(`output2: ${output2}`);
+// console.log(`expectedOutput2: ${expected2}`);
