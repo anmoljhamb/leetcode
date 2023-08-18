266903ab4f902f02b2c53a5e04813898dbabbf94
Anmol Jhamb <talktoanmol@outlook.com>
2023-08-18 07:46:03 +0530

    modify the code to have a better run time

--- 053_max_subarray.ts
+++ 053_max_subarray.ts
@@ -1,28 +1,14 @@
-// KADANE'S ALGO
-function allNegatvie(arr: number[]): boolean {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > 0) return false;
-    }
-    return true;
-}
-
 function maxSubArray(arr: number[]): number {
     let sumSoFar = 0;
-    let maxSum = 0;
+    let maxSum = -Infinity;
+    let maxSoFar = -Infinity;
     for (let i = 0; i < arr.length; i++) {
         sumSoFar += arr[i];
-        if (sumSoFar > maxSum) {
-            maxSum = sumSoFar;
-        }
+        maxSum = Math.max(sumSoFar, maxSum);
+        maxSoFar = Math.max(maxSoFar, arr[i]);
         if (sumSoFar < 0) {
             sumSoFar = 0;
         }
-    }
-    if (allNegatvie(arr)) {
-        maxSum = -Infinity;
-        arr.forEach((num) => {
-            maxSum = Math.max(maxSum, num);
-        });
     }
     return maxSum;
 }
