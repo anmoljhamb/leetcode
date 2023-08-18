d4477b95ebc524df8cc9d42a2307c4ea8818fdb1
Anmol Jhamb <talktoanmol@outlook.com>
2023-08-18 10:09:41 +0530

    first solution for trapping water

--- 042_trapping_water.ts
+++ 042_trapping_water.ts
@@ -1,5 +1,29 @@
-function trap(height: number[]): number {
-    return 0;
+function util(arr: number[], low: number, high: number) {
+    let sum = 0;
+    const k = Math.min(arr[low], arr[high]);
+    for (let i = low + 1; i < high; i++) {
+        sum += k - arr[i];
+    }
+    return sum;
+}
+
+function trap(arr: number[]): number {
+    let i = 0;
+    let ans = 0;
+    while (i < arr.length - 1) {
+        // console.log(`Finding the next index for ${i}`);
+        let j = i + 1;
+        // Find the first j st arr[j] >= arr[i]
+        while (j < arr.length && arr[j] < arr[i]) j++;
+        if (j !== arr.length) {
+            ans += util(arr, i, j);
+            // console.log(`(${i}, ${j})`);
+            i = j;
+        } else {
+            i++;
+        }
+    }
+    return ans;
 }
 
 export default trap;
--- driver.ts
+++ driver.ts
@@ -1,6 +1,10 @@
 import solution from "./042_trapping_water";
 
-const input1 = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+// const input1 = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+// const output1 = solution(input1);
+// const expected1 = 6;
+
+const input1 = [10, 3, 6, 7];
 const output1 = solution(input1);
 const expected1 = 6;
 
