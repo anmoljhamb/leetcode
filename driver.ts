6bd44a6f9fea13b039f95f5432ab5d99acfa345f
Anmol Jhamb <talktoanmol@outlook.com>
2023-08-18 08:57:15 +0530

    driver code added for hard problems

---
+++ driver.ts
@@ -0,0 +1,9 @@
+import solution from "./042_trapping_water";
+
+const input1 = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+const output1 = solution(input1);
+const expected1 = 6;
+
+console.log(`input1: ${input1}`);
+console.log(`output1: ${output1}`);
+console.log(`expectedOutput1: ${expected1}`);
