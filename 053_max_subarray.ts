d22ed7453c3ce9ffc007e35161828cbf039af875
Anmol Jhamb <talktoanmol@outlook.com>
2023-08-18 07:50:10 +0530

    best solution so far for maxSubarray

--- 053_max_subarray.ts
+++ 053_max_subarray.ts
@@ -1,11 +1,9 @@
 function maxSubArray(arr: number[]): number {
     let sumSoFar = 0;
     let maxSum = -Infinity;
-    let maxSoFar = -Infinity;
     for (let i = 0; i < arr.length; i++) {
         sumSoFar += arr[i];
         maxSum = Math.max(sumSoFar, maxSum);
-        maxSoFar = Math.max(maxSoFar, arr[i]);
         if (sumSoFar < 0) {
             sumSoFar = 0;
         }
