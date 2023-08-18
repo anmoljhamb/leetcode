d27cd3835f1e01835cccccd82a49433b95664e58
Anmol Jhamb <talktoanmol@outlook.com>
2023-08-18 07:38:33 +0530

    pivot index

--- 074_pivot_index.ts
+++ 074_pivot_index.ts
@@ -1,12 +1,22 @@
 function pivotIndex(arr: number[]): number {
-    let minSoFar = Infinity;
-    let maxOfRest = -Infinity;
-
-    let minArrs
-
-    for (let i = 0; i < arr.length; i++) {
-        minSoFar = Math.min(minSoFar, arr[i]);
-        maxSoFar = Math.max(maxSoFar, arr[i]);
-        if ( minSoFar)
+    const prefixSum = [...arr];
+    for (let i = 1; i < prefixSum.length; i++) {
+        prefixSum[i] += prefixSum[i - 1];
     }
+    const totalSum = prefixSum[prefixSum.length - 1];
+    // console.log(`totalSum: ${totalSum}`);
+    for (let i = 0; i < prefixSum.length; i++) {
+        // console.log(
+        //     `i=${i} prefixSum[i-1]=${prefixSum[i - 1]}, prefixSum[i]=${
+        //         prefixSum[i]
+        //     } totalSum-prefixSum[i]=${totalSum - prefixSum[i]}`
+        // );
+        if (i === 0 && 0 === totalSum - prefixSum[i]) {
+            return i;
+        }
+        if (prefixSum[i - 1] === totalSum - prefixSum[i]) {
+            return i;
+        }
+    }
+    return -1;
 }
