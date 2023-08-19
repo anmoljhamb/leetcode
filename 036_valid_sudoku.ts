4967ea4c3a22ce015764f72979350bea9c4b21fe
Anmol Jhamb <talktoanmol@outlook.com>
2023-08-19 23:25:09 +0530

    only loops through the entire array once. and uses sets

--- 036_valid_sudoku.ts
+++ 036_valid_sudoku.ts
@@ -1,59 +1,39 @@
 const N = 9;
 const n = 3;
 
-function getBox(board: string[][], i: number, j: number) {
-    const temp: string[] = [];
-    const x = Math.floor(i / n) * n;
-    const y = Math.floor(j / n) * n;
-    for (let low = x; low < x + n; low++) {
-        for (let high = y; high < y + n; high++) {
-            temp.push(board[low][high]);
+function isValidSudoku(board: string[][]): boolean {
+    const row: Record<string, Set<string>> = {};
+    const col: Record<string, Set<string>> = {};
+    const box: Record<string, Set<string>> = {};
+
+    for (let i = 0; i < N; i++) {
+        row[i] = new Set<string>();
+    }
+
+    for (let i = 0; i < N; i++) {
+        col[i] = new Set<string>();
+    }
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            box[`${i}${j}`] = new Set<string>();
         }
     }
-    return temp;
-}
 
-function getRow(board: string[][], i: number, j: number) {
-    const temp: string[] = [];
-    for (let low = 0; low < N; low++) {
-        temp.push(board[i][low]);
-    }
-    return temp;
-}
-
-function getCol(board: string[][], i: number, j: number) {
-    const temp: string[] = [];
-    for (let low = 0; low < N; low++) {
-        temp.push(board[low][j]);
-    }
-    return temp;
-}
-
-function containsDuplicates(arr: string[]) {
-    arr = arr.filter((x) => x !== ".");
-    return arr.length !== new Set(arr).size;
-}
-
-function isValidSudoku(board: string[][]): boolean {
     for (let i = 0; i < N; i++) {
         for (let j = 0; j < N; j++) {
-            if (board[i][j] === ".") continue;
-            const x = getBox(board, i, j);
-            const y = getRow(board, i, j);
-            const z = getCol(board, i, j);
+            const curr = board[i][j];
+            const temp = `${Math.floor(i / n)}${Math.floor(j / n)}`;
 
-            // console.log(`${i}, ${j}`);
-            // console.log(x);
-            // console.log(y);
-            // console.log(z);
+            if (curr === ".") continue;
 
-            if (
-                containsDuplicates(x) ||
-                containsDuplicates(y) ||
-                containsDuplicates(z)
-            ) {
+            if (row[i].has(curr) || col[j].has(curr) || box[temp].has(curr)) {
                 return false;
             }
+
+            row[i].add(curr);
+            col[j].add(curr);
+            box[temp].add(curr);
         }
     }
 
