d6e50506d725c48ab74533dbc69939cb73952a3a
Anmol Jhamb <talktoanmol@outlook.com>
2023-08-19 22:47:07 +0530

    O(n) solution for top k freq

--- 347_top_k_freq.ts
+++ 347_top_k_freq.ts
@@ -6,8 +6,27 @@ function topKFrequent(arr: number[], k: number): number[] {
         else freq[num] = 1;
     });
 
-    return Object.keys(freq)
-        .sort((a, b) => freq[b] - freq[a])
-        .slice(0, k)
-        .map((key) => Number.parseInt(key));
+    const temp: number[][] = [...new Array(arr.length + 1)].map(() => []);
+
+    // console.log(freq);
+
+    Object.keys(freq).forEach((key) => {
+        temp[freq[key]].push(Number.parseInt(key));
+    });
+
+    // console.log(temp);
+
+    let ans: number[] = [];
+
+    for (let i = temp.length - 1; i > 0 && k > 0; i--) {
+        if (temp[i].length > k) {
+            ans = [...ans, ...temp[i].slice(0, k)];
+            k = 0;
+        } else {
+            ans = [...ans, ...temp[i]];
+            k -= temp[i].length;
+        }
+    }
+
+    return ans;
 }
