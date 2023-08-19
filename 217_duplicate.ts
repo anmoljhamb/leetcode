202bc1d2760028b3746f884981b4039e3971a2b5
Anmol Jhamb <talktoanmol@outlook.com>
2023-08-19 13:50:42 +0530

    contains duplicate

--- 217_duplicate.ts
+++ 217_duplicate.ts
@@ -1,11 +1,5 @@
 function containsDuplicate(arr: number[]): boolean {
-    const freq: Record<number, number> = {};
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] in freq) {
-            return true;
-        } else {
-            freq[arr[i]] = 1;
-        }
-    }
-    return false;
+    const set = new Set<number>();
+    arr.forEach((num) => set.add(num));
+    return set.size !== arr.length;
 }
