f585533a4913ad9d6089dfc21849b070ff27c3cb
Anmol Jhamb <talktoanmol@outlook.com>
2023-08-19 14:24:47 +0530

    valid anagram approach modified

--- 242_valid_anagram.ts
+++ 242_valid_anagram.ts
@@ -1,31 +1,12 @@
+function makeKey(str: string): string {
+    const temp = "a".charCodeAt(0);
+    const count = new Array(26).fill(0);
+    for (let i = 0; i < str.length; i++) {
+        count[str.charCodeAt(i) - temp] += 1;
+    }
+    return count.join("-");
+}
+
 function isAnagram(s: string, t: string): boolean {
-    const freq1: Record<string, number> = {};
-    const freq2: Record<string, number> = {};
-
-    if (s.length !== t.length) {
-        return false;
-    }
-
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] in freq1) {
-            freq1[s[i]] += 1;
-        } else {
-            freq1[s[i]] = 1;
-        }
-    }
-
-    for (let i = 0; i < s.length; i++) {
-        if (t[i] in freq2) {
-            freq2[t[i]] += 1;
-        } else {
-            freq2[t[i]] = 1;
-        }
-    }
-
-    console.log(freq1);
-    console.log(freq2);
-
-    return Object.keys(freq1).reduce((prev, curr) => {
-        return prev && freq1[curr] === freq2[curr];
-    }, true);
+    return makeKey(s) === makeKey(t);
 }
