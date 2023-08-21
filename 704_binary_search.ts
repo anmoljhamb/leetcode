0d5b9e7cc40d65e09d387c80fbbe968d6513642c
Anmol Jhamb <talktoanmol@outlook.com>
2023-08-21 06:09:34 +0530

    binary_search

---
+++ 704_binary_search.ts
@@ -0,0 +1,18 @@
+function search(arr: number[], target: number): number {
+    let low = 0,
+        high = arr.length - 1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] > target) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return -1;
+}
