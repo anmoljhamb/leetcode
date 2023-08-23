ab36b264c070eefce815d4376a75539711366fd6
Anmol Jhamb <talktoanmol@outlook.com>
2023-08-23 23:25:11 +0530

    O(n) solution for 003 longest substring

---
+++ 003_longest_substr.ts
@@ -0,0 +1,28 @@
+function lengthOfLongestSubstring(s: string): number {
+    let left = 0,
+        right = 0;
+
+    let maxLength = 0;
+
+    const set = new Set<string>();
+
+    while (right < s.length) {
+        if (!set.has(s[right])) {
+            set.add(s[right]);
+            right++;
+        } else {
+            while (true) {
+                if (s[left] === s[right]) {
+                    left++;
+                    break;
+                }
+                set.delete(s[left++]);
+            }
+            set.add(s[right]);
+            right++;
+        }
+        maxLength = Math.max(maxLength, set.size);
+    }
+
+    return maxLength;
+}
