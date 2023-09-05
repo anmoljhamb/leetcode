3146d4992e0cd41836e43d4380f2f77d58092f24
Anmol Jhamb <talktoanmol@outlook.com>
2023-09-05 06:46:56 +0530

    merge k sorted lists

--- 023_merge_k_sorted.ts
+++ 023_merge_k_sorted.ts
@@ -7,23 +7,60 @@ class ListNode {
     }
 }
 
-function merge(
-    lists: Array<ListNode | null>,
-    low: number,
-    high: number,
-    ans: ListNode | null
-) {
-    if (low <= high) {
-        return;
-    }
-    const mid = Math.floor((low + high) / 2);
+function mergeTwoLists(
+    list1: ListNode | null,
+    list2: ListNode | null
+): ListNode | null {
+    const dummy1 = new ListNode(0, list1);
+    let prev: ListNode | null = dummy1;
 
-    merge(lists, low, mid, ans);
-    merge(lists, mid + 1, high, ans);
+    while (list1 !== null && list2 !== null) {
+        if (list1.val <= list2.val) {
+            list1 = list1.next;
+            prev = prev!.next;
+        } else {
+            let temp = list2.next;
+            prev!.next = list2;
+            list2.next = list1;
+            list2 = temp;
+            list1 = prev!.next;
+        }
+    }
+
+    while (list2) {
+        prev!.next = list2;
+        prev = prev!.next;
+        list2 = list2.next;
+    }
+
+    return dummy1.next;
+}
+
+function printLL(head: ListNode | null): void {
+    let curr = head;
+    const arr: number[] = [];
+    while (curr) {
+        arr.push(curr.val);
+        curr = curr.next;
+    }
+    console.log(arr);
 }
 
 function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
-    const dummy = new ListNode();
-    merge(lists, 0, lists.length - 1, dummy);
-    return dummy.next;
+    if (lists.length === 0) return null;
+
+    while (lists.length > 1) {
+        const temp: Array<ListNode | null> = [];
+        for (let i = 0; i < lists.length; i += 2) {
+            temp.push(
+                mergeTwoLists(
+                    lists[i],
+                    i + 1 < lists.length ? lists[i + 1] : null
+                )
+            );
+        }
+        lists = temp;
+    }
+
+    return lists[0];
 }
