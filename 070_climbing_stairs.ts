5927a03ab08d4167b2576174ba1d48302a1301e0
Anmol Jhamb <talktoanmol@outlook.com>
2023-09-10 14:10:15 +0530

    climb stairs tabular dp

--- 070_climbing_stairs.ts
+++ 070_climbing_stairs.ts
@@ -1,8 +1,21 @@
 // Memoization DP
 
-function climbStairs(n: number, memo: Record<number, number> = {}): number {
-    if (n in memo) return memo[n];
-    if (n <= 2) return n;
-    memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
-    return memo[n];
+// function climbStairs(n: number, memo: Record<number, number> = {}): number {
+//     if (n in memo) return memo[n];
+//     if (n <= 2) return n;
+//     memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
+//     return memo[n];
+// }
+
+// Tabular Form
+function climbStairs(n: number) {
+    const dp: number[] = new Array(n + 1).fill(0);
+    dp[0] = 1;
+
+    for (let i = 0; i <= n; i++) {
+        dp[i + 1] += dp[i];
+        dp[i + 2] += dp[i];
+    }
+
+    return dp[n];
 }
