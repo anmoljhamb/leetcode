ce2fe33f37f872726a870cee8c8cb7da66034944
Anmol Jhamb <talktoanmol@outlook.com>
2023-09-12 09:56:25 +0530

    tabular form for 746 min cost

--- 746_min_cost_climb_stairs_tab.ts
+++ 746_min_cost_climb_stairs_tab.ts
@@ -1,9 +1,20 @@
 function minCostClimbingStairs(cost: number[]): number {
+    cost.unshift(0);
     cost.push(0);
 
-    for (let i = cost.length - 3; i >= 0; i--) {
-        cost[i] = Math.min(cost[i] + cost[i + 1], cost[i] + cost[i + 2]);
+    const dp: number[] = new Array(cost.length + 1).fill(0);
+
+    console.log(cost);
+
+    for (let i = 1; i < cost.length; i++) {
+        dp[i] = cost[i] + Math.min(dp[i - 1], i - 2 >= 0 ? dp[i - 2] : 0);
     }
 
-    return Math.min(cost[0], cost[1]);
+    console.log(dp);
+
+    return dp[cost.length - 1];
 }
+
+console.log(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]));
+console.log(minCostClimbingStairs([10, 15, 20]));
+console.log(minCostClimbingStairs([12, 100]));
