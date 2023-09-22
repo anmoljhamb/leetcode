ee6f360c7ebd74b5d57a431eb62bf99f2021a08f
Anmol Jhamb <talktoanmol@outlook.com>
2023-09-22 21:58:51 +0530

    using a two pointer approach

--- 392_subsequence.ts
+++ 392_subsequence.ts
@@ -1,37 +1,21 @@
-function isSubsequence(s: string, t: string, low = 0, prev = ""): boolean {
-    const freq: Record<string, number[]> = {};
-    for (let i = 0; i < t.length; i++) {
-        const char = t[i];
-        if (char in freq) {
-            freq[char].push(i);
-        } else {
-            freq[char] = [i];
-        }
+function isSubsequence(s: string, t: string): boolean {
+    let left = 0,
+        right = 0;
+
+    while (left < s.length && right < t.length) {
+        while (right < t.length && s[left] !== t[right++]) {}
+        left++;
     }
 
-    let prevIndex = -Infinity;
-
-    for (const char of s) {
-        if (!(char in freq)) {
-            return false;
-        }
-
-        if (freq[char].length === 0) return false;
-
-        let temp = freq[char][0];
-        while (freq[char].length > 0 && temp < prevIndex) {
-            temp = freq[char].shift() as number;
-        }
-
-        if (temp < prevIndex) {
-            return false;
-        }
-
-        prevIndex = temp;
-        freq[char].shift();
+    if (left === s.length && right === t.length) {
+        if (s[left - 1] === t[right - 1]) return true;
     }
 
-    return true;
+    return right < t.length;
 }
 
+console.log(isSubsequence("xab", "baab"));
 console.log(isSubsequence("ab", "baab"));
+console.log(isSubsequence("aaaaaa", "bbaaaa"));
+console.log(isSubsequence("abc", "abhgdc"));
+console.log(isSubsequence("a", "b"));
