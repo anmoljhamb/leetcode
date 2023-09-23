941a5dd6b562518af0bb35914fcee46c1d253838
Anmol Jhamb <talktoanmol@outlook.com>
2023-09-23 23:00:24 +0530

    coin change using recursion

--- 322_coin_change.ts
+++ 322_coin_change.ts
@@ -1,19 +1,26 @@
-function coinChange(coins: number[], amount: number): number {
-    coins.sort((a, b) => b - a);
-    let count = 0;
+function coinChange(
+    coins: number[],
+    amount: number,
+    memo: Record<number, number> = {}
+): number {
+    if (amount in memo) return memo[amount];
+    if (amount === 0) return 0;
+    if (amount < 0) return -1;
 
-    for (let i = 0; i < coins.length && amount > 0; i++) {
-        const prev = amount;
-        amount = amount % coins[i];
-        if (prev !== amount) {
-            count += Math.floor(prev / coins[i]);
+    let shortestCoin = Infinity;
+    for (let coin of coins) {
+        const remainder = amount - coin;
+        let result = coinChange(coins, remainder, memo);
+        if (result !== -1) {
+            result += 1;
+            shortestCoin = Math.min(result, shortestCoin);
         }
     }
 
-    if (amount === 0) return count;
-    return -1;
+    memo[amount] = shortestCoin === Infinity ? -1 : shortestCoin;
+    return memo[amount];
 }
 
 console.log(coinChange([1, 2, 5], 11));
 console.log(coinChange([2], 3));
-console.log(coinChange([1], 0));
+console.log(coinChange([1], 10000));
