a7b3fe7188ae44425e0a0a37712e30df1c272271
Anmol Jhamb <talktoanmol@outlook.com>
2023-09-25 10:26:38 +0530

    add the ascii values of both strings, and return the letters[index]

--- 389_find_the_diff.ts
+++ 389_find_the_diff.ts
@@ -2,24 +2,18 @@ const charCode = (s: string) => {
     return s.charCodeAt(0) - "a".charCodeAt(0);
 };
 
-const hashmap = (s: string) => {
-    const hash: number[] = new Array(26).fill(0);
-    for (const char of s) {
-        hash[charCode(char)]++;
-    }
-    return hash;
-};
-
 function findTheDifference(s: string, t: string): string {
     const letters = "abcdefghijklmnopqrstuvwxyz";
-    const hash1 = hashmap(s);
-    const hash2 = hashmap(t);
 
-    for (let i = 0; i < 26; i++) {
-        if (hash1[i] !== hash2[i]) {
-            return letters[i];
-        }
+    let sum1 = 0;
+    for (const char of s) {
+        sum1 += charCode(char);
     }
 
-    return "a";
+    let sum2 = 0;
+    for (const char of t) {
+        sum2 += charCode(char);
+    }
+
+    return letters[sum2 - sum1];
 }
