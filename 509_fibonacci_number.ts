13e8238359ea616a92cdb710e007416f45bff77f
Anmol Jhamb <talktoanmol@outlook.com>
2023-09-30 08:46:12 +0530

    509 fibonacci

---
+++ 509_fibonacci_number.ts
@@ -0,0 +1,6 @@
+function fib(n: number, memo: Record<number, number> = {}): number {
+    if (n in memo) return memo[n];
+    if (n < 2) return n;
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+    return memo[n];
+}
