628a4041bb0666836b68084d787fa22da5317cba
Anmol Jhamb <talktoanmol@outlook.com>
2023-11-24 14:08:45 +0530

    190_rev_bit

---
+++ 190_rev_bit.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ans = 0
+        for _ in range(32):
+            ans |= n&1
+            ans = ans << 1
+            n = n >> 1
+        ans = ans >> 1
+        return ans
+        
