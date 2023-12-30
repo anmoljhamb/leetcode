eeb78beabffed868f2f90e49b6aa9e0194ac28a0
Anmol Jhamb <talktoanmol@outlook.com>
2023-12-30 08:08:54 +0530

    Functional Recursion

--- recursion/sum_till_n.ts
+++ recursion/sum_till_n.ts
@@ -9,3 +9,14 @@ function sum_till_n_1(n: number, sum: number = 0) {
 }
 
 sum_till_n_1(10);
+
+// Functional Recursion
+
+function sum_till_n_2(n: number) {
+    if (n < 1) {
+        return 0;
+    }
+    return n + sum_till_n_2(n - 1);
+}
+
+console.log(sum_till_n_2(10));
