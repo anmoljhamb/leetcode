62472c7dd4812c7193e859e40413cbe463a672ac
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-04 08:46:39 +0530

    Solved problem of the day

---
+++ 2870_minimum_operations.ts
@@ -0,0 +1,23 @@
+const helper = (x: number) => {
+  if (x % 3 === 0) return x / 3;
+  if (x % 3 === 2) return 1 + (x - 2) / 3;
+  return 2 + (x - 4) / 3;
+};
+
+function minOperations(arr: number[]): number {
+  const freq: Record<string, number> = {};
+  arr.forEach((num) => {
+    if (num in freq) freq[num]++;
+    else freq[num] = 1;
+  });
+  let ans = 0;
+  const keys: (keyof Record<string, number>)[] = Object.keys(freq);
+  for (let i = 0; i < keys.length; i++) {
+    const currFreq = freq[keys[i]];
+    if (currFreq < 2) return -1;
+    ans += helper(currFreq);
+  }
+  return ans;
+}
+
+console.log(minOperations([1, 1, 2, 3, 2, 3]));
