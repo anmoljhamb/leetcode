5264da55650e46536395ac5140072d701b01f409
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-19 08:49:54 +0530

    Recursive solution - 746

--- 746_min_cost_climb_stairs.ts
+++ 746_min_cost_climb_stairs.ts
@@ -1,23 +1,11 @@
-function helper(
-    low: number,
-    arr: number[],
-    memo: Record<number, number> = {}
-): number {
-    if (low in memo) return memo[low];
-    if (low >= arr.length) return 0;
-
-    const temp1 = helper(low + 1, arr, memo);
-    const temp2 = helper(low + 2, arr, memo);
-
-    memo[low] = arr[low] + Math.min(temp1, temp2);
-    return memo[low];
+function recurse(arr: number[], i: number): number {
+  if (i === 0 || i === 1) return 0;
+  return Math.min(
+    arr[i - 1] + recurse(arr, i - 1),
+    arr[i - 2] + recurse(arr, i - 2),
+  );
 }
 
 function minCostClimbingStairs(cost: number[]): number {
-    cost.unshift(0);
-    return helper(0, cost);
+  return recurse(cost, cost.length);
 }
-
-minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]);
-minCostClimbingStairs([10, 15, 20]);
-minCostClimbingStairs([12, 100]);
