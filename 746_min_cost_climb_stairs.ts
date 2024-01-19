13bd5df9719438030bbb4e686be92472b433293f
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-19 08:59:47 +0530

    746: Removed extra function.

--- 746_min_cost_climb_stairs.ts
+++ 746_min_cost_climb_stairs.ts
@@ -1,6 +1,6 @@
-function optimized(arr: number[]) {
-  let prev1 = 0;
+function minCostClimbingStairs(arr: number[]): number {
   let prev2 = 0;
+  let prev1 = 0;
   let ans = 0;
   for (let i = 2; i <= arr.length; i++) {
     ans = Math.min(arr[i - 1] + prev1, arr[i - 2] + prev2);
@@ -8,8 +8,4 @@ function optimized(arr: number[]) {
     prev1 = ans;
   }
   return ans;
-}
-
-function minCostClimbingStairs(cost: number[]): number {
-  return optimized(cost);
 }
