69fa5c89ab87a51d28a5fb791940803892476c06
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-19 08:16:13 +0530

    POTD: Tabular Method.

--- 931_min_falling_path.ts
+++ 931_min_falling_path.ts
@@ -1,23 +1,30 @@
-function helper(mat: number[][], i: number, j: number): number {
-  if (j < 0 || j >= mat.length) return Infinity;
-  if (i === mat.length - 1) return mat[i][j];
+function table(mat: number[][]) {
+  const n = mat.length;
+  const dp = new Array<number[]>(n);
+  for (let i = 0; i < n; i++) {
+    dp[i] = new Array(n).fill(Infinity);
+  }
 
-  const curr =
-    mat[i][j] +
-    Math.min(
-      helper(mat, i + 1, j - 1),
-      helper(mat, i + 1, j),
-      helper(mat, i + 1, j + 1),
-    );
+  for (let j = 0; j < n; j++) {
+    dp[n - 1][j] = mat[n - 1][j];
+  }
+  for (let i = n - 2; i > -1; i--) {
+    for (let j = 0; j < n; j++) {
+      const left = j > 0 ? dp[i + 1][j - 1] : Infinity;
+      const bottom = dp[i + 1][j];
+      const right = j < n - 1 ? dp[i + 1][j + 1] : Infinity;
 
-  return curr;
+      dp[i][j] = mat[i][j] + Math.min(left, bottom, right);
+    }
+  }
+
+  let ans = Infinity;
+  for (let i = 0; i < n; i++) {
+    ans = Math.min(ans, dp[0][i]);
+  }
+  return ans;
 }
 
 function minFallingPathSum(mat: number[][]): number {
-  let ans = Infinity;
-  for (let i = 0; i < mat.length; i++) {
-    const curr = helper(mat, 0, i);
-    ans = Math.min(ans, curr);
-  }
-  return ans;
+  return table(mat);
 }
