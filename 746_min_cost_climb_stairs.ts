04ae778f81b74a56ed7da044aa3e6b7cd1d64bb3
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-19 08:55:42 +0530

    746: Tabular method

--- 746_min_cost_climb_stairs.ts
+++ 746_min_cost_climb_stairs.ts
@@ -1,11 +1,13 @@
-function recurse(arr: number[], i: number): number {
-  if (i === 0 || i === 1) return 0;
-  return Math.min(
-    arr[i - 1] + recurse(arr, i - 1),
-    arr[i - 2] + recurse(arr, i - 2),
-  );
+function tab(arr: number[]) {
+  const dp = new Array<number>(arr.length + 1);
+  dp[0] = 0;
+  dp[1] = 0;
+  for (let i = 2; i <= arr.length; i++) {
+    dp[i] = Math.min(arr[i - 1] + dp[i - 1], arr[i - 2] + dp[i - 2]);
+  }
+  return dp[arr.length];
 }
 
 function minCostClimbingStairs(cost: number[]): number {
-  return recurse(cost, cost.length);
+  return tab(cost);
 }
