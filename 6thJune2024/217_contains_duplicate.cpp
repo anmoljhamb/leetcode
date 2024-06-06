9cdeacd3e1bf95fc23251bc4874c4c0a1eae7d0a
Anmol Jhamb <talktoanmol@outlook.com>
2024-06-06 09:44:32 +0530

    217 cpp, using hashmap

--- 6thJune2024/217_contains_duplicate.cpp
+++ 6thJune2024/217_contains_duplicate.cpp
@@ -1,18 +1,34 @@
 #include <bits/stdc++.h>
 #include <set>
+#include <unordered_map>
 using namespace std;
+
+// class Solution {
+// public:
+//   bool containsDuplicate(vector<int> &nums) {
+//     // This algorithm is O(nlogn) since, the count function is O(logn)
+//     set<int> my_set;
+//     for (auto num : nums) {
+//       if (my_set.count(num) == 1) {
+//         return true;
+//       }
+//       my_set.insert(num);
+//     }
+//     return false;
+//   }
+// };
 
 class Solution {
 public:
   bool containsDuplicate(vector<int> &nums) {
-    // This algorithm is O(nlogn) since, the count function is O(logn)
-    set<int> my_set;
-    for (auto num : nums) {
-      if (my_set.count(num) == 1) {
+    unordered_map<int, int> _map;
+    for (const auto num : nums) {
+      if (_map.find(num) != _map.end()) {
         return true;
       }
-      my_set.insert(num);
+      _map[num] = 1;
     }
+
     return false;
   }
 };
