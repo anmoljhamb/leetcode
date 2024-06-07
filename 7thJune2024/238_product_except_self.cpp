8a3a0c5e6027de483e9ad0461fa13320e9e89b70
Anmol Jhamb <talktoanmol@outlook.com>
2024-06-07 10:37:56 +0530

    without using extra space

--- 7thJune2024/238_product_except_self.cpp
+++ 7thJune2024/238_product_except_self.cpp
@@ -4,20 +4,17 @@ using namespace std;
 class Solution {
 public:
   vector<int> productExceptSelf(vector<int> &nums) {
-    vector<int> left = nums, right = nums, ans = nums;
-    for (int i = 1; i < left.size(); i++) {
-      left[i] *= left[i - 1];
+    vector<int> ans(nums.size());
+    int left = 1;
+    for (int i = 0; i < nums.size(); i++) {
+      ans[i] = left;
+      left *= nums[i];
     }
-    for (int i = right.size() - 2; i >= 0; i--) {
-      right[i] *= right[i + 1];
+    int right = 1;
+    for (int i = nums.size() - 1; i >= 0; i--) {
+      ans[i] *= right;
+      right *= nums[i];
     }
-
-    for (int i = 0; i < ans.size(); i++) {
-      int _left = i == 0 ? 1 : left[i - 1];
-      int _right = i == ans.size() - 1 ? 1 : right[i + 1];
-      ans[i] = _left * _right;
-    }
-
     return ans;
   }
 };
