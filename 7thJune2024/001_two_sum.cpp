6f839543698e87021bbd728c18d76d982e38ca44
Anmol Jhamb <talktoanmol@outlook.com>
2024-06-07 07:52:31 +0530

    001, two_sum O(n)

--- 7thJune2024/001_two_sum.cpp
+++ 7thJune2024/001_two_sum.cpp
@@ -1,17 +1,32 @@
 #include <bits/stdc++.h>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
 public:
   vector<int> twoSum(vector<int> &nums, int target) {
     vector<int> ans;
+    unordered_map<int, vector<int>> hash;
+    int temp;
     for (int i = 0; i < nums.size(); i++) {
-      for (int j = i + 1; j < nums.size(); j++) {
-        if (nums[i] + nums[j] == target) {
-          ans.push_back(i);
-          ans.push_back(j);
+      if (hash.count(nums[i]) == 0) {
+        hash[nums[i]] = {i};
+      } else {
+        hash[nums[i]].push_back(i);
+      }
+    }
+    for (int i = 0; i < nums.size(); i++) {
+      temp = target - nums[i];
+      if (temp == nums[i]) {
+        if (hash[nums[i]].size() > 1) {
+          ans = {hash[nums[i]][0], hash[nums[i]][1]};
           return ans;
         }
+        continue;
+      }
+      if (hash.count(temp) == 1) {
+        ans = {i, hash[temp][0]};
+        return ans;
       }
     }
     return ans;
