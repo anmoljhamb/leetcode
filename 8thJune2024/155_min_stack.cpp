740721ca474eff74bc81ff83aedb8c14b9313c25
Anmol Jhamb <talktoanmol@outlook.com>
2024-06-08 14:15:36 +0530

    O(1) for every operation

--- 8thJune2024/155_min_stack.cpp
+++ 8thJune2024/155_min_stack.cpp
@@ -1,38 +1,32 @@
 #include <algorithm>
 #include <bits/stdc++.h>
-#include <cstdint>
 using namespace std;
 
 class MinStack {
   stack<int> s;
-  int min_el = INT32_MAX;
+  stack<int> min_stack;
 
 public:
   MinStack() {}
 
   void push(int val) {
     s.push(val);
-    min_el = min(min_el, val);
+    if (min_stack.size() == 0) {
+      min_stack.push(val);
+      return;
+    }
+    min_stack.push(min(val, min_stack.top()));
   }
 
   void pop() {
-    if (s.top() != min_el) {
-      s.pop();
-      return;
-    }
+    int temp = s.top();
     s.pop();
-    stack<int> temp = s;
-    int temp_mini = INT32_MAX;
-    while (!temp.empty()) {
-      temp_mini = min(temp.top(), temp_mini);
-      temp.pop();
-    }
-    min_el = temp_mini;
+    min_stack.pop();
   }
 
   int top() { return s.top(); }
 
-  int getMin() { return min_el; }
+  int getMin() { return min_stack.top(); }
 };
 
 /**
