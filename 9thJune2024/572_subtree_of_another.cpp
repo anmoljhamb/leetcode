0e679745e8db73b170601c062da5e9fcbcf773dd
Anmol Jhamb <talktoanmol@outlook.com>
2024-06-09 11:33:34 +0530

    subtree of another subtree in linear time

--- 9thJune2024/572_subtree_of_another.cpp
+++ 9thJune2024/572_subtree_of_another.cpp
@@ -24,15 +24,54 @@ public:
     return left + "-" + right + "-" + to_string(root->val) + "-";
   }
 
-  bool isSubstr(string str1, string str2) {
-    return str1.find(str2) != string::npos;
+  bool isSubstr(string haystack, string needle) {
+    const int n = haystack.size(), m = needle.size();
+    vector<int> lps(m, 0);
+
+    int i = 0, j = 0, prevLps = 0;
+
+    i = 1;
+
+    while (i < m) {
+      if (needle[i] == needle[prevLps]) {
+        lps[i] = prevLps + 1;
+        prevLps = lps[i];
+        i++;
+        continue;
+      }
+
+      if (prevLps == 0) {
+        lps[i] = 0;
+        i++;
+        continue;
+      }
+
+      prevLps = lps[prevLps - 1];
+    }
+
+    i = 0, j = 0;
+
+    while (i < n) {
+      if (haystack[i] == needle[j]) {
+        i++;
+        j++;
+      } else if (j == 0) {
+        i++;
+      } else {
+        j = lps[j - 1];
+      }
+
+      if (j == m) {
+        return true;
+      }
+    }
+
+    return false;
   }
 
   bool isSubtree(TreeNode *root, TreeNode *subRoot) {
     string str1 = serialize(root);
     string str2 = serialize(subRoot);
-    cout << str1 << endl;
-    cout << str2 << endl;
     return isSubstr(str1, str2);
   }
 };
