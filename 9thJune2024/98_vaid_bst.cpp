2a87bae71635e1421f7ee2af3b5bcf078d1797d0
Anmol Jhamb <talktoanmol@outlook.com>
2024-06-09 12:33:27 +0530

    Change boundaries to long, to carry extra 1

--- 9thJune2024/98_vaid_bst.cpp
+++ 9thJune2024/98_vaid_bst.cpp
@@ -13,11 +13,14 @@ struct TreeNode {
       : val(x), left(left), right(right) {}
 };
 
+long MAX = (long)INT32_MAX + 1;
+long MIN = (long)INT32_MIN - 1;
+
 class Solution {
 
 public:
-  bool isValidBST(TreeNode *root, int left_boundary = INT32_MIN,
-                  int right_boundary = INT32_MAX) {
+  bool isValidBST(TreeNode *root, long left_boundary = MIN,
+                  long right_boundary = MAX) {
     if (!root)
       return true;
     if (right_boundary <= root->val || left_boundary >= root->val)
