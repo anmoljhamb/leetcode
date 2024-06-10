5f75c5eda516530e4de3155483ea4ddab651cbcc
Anmol Jhamb <talktoanmol@outlook.com>
2024-06-10 14:59:22 +0530

    next smaller done

--- 10thJune2024/prev_smallest_element.cpp
+++ 10thJune2024/prev_smallest_element.cpp
@@ -27,12 +27,44 @@ vector<int> pse(vector<int> arr) {
   return ans;
 }
 
-int main() {
-  vector<int> arr = {2, 5, 3, 6, 7, 4, 8, 1, 3};
-  vector<int> ans = pse(arr);
-  for (auto x : ans) {
+vector<int> nse(vector<int> arr) {
+  stack<int> s;
+  vector<int> ans(arr.size());
+
+  int i = arr.size() - 1;
+  while (i > -1) {
+    if (s.empty()) {
+      ans[i] = -1;
+    } else {
+      while (!s.empty() && s.top() > arr[i]) {
+        s.pop();
+      }
+      if (s.empty()) {
+        ans[i] = -1;
+      } else {
+        ans[i] = s.top();
+      }
+    }
+    s.push(arr[i]);
+    i--;
+  }
+
+  return ans;
+}
+
+void print(vector<int> arr) {
+  for (auto x : arr) {
     cout << x << " ";
   }
   cout << endl;
+}
+
+int main() {
+  vector<int> arr = {2, 5, 3, 6, 7, 4, 8, 1, 3};
+  print(arr);
+  vector<int> _pse = pse(arr);
+  print(_pse);
+  vector<int> _nse = nse(arr);
+  print(_nse);
   return 0;
 }
