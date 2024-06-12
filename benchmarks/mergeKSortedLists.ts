8051954901d58ae352c26296382c18edc35d4869
Anmol Jhamb <talktoanmol@outlook.com>
2024-06-12 11:17:04 +0530

    benchmark done

--- benchmarks/mergeKSortedLists.ts
+++ benchmarks/mergeKSortedLists.ts
@@ -25,14 +25,14 @@ function mergeTwo(list1: ListNode | null, list2: ListNode | null) {
 
   let count = 0;
 
-  while (list1 && list2) {
+  while (list1 != null && list2 !== null) {
     count++;
     if (list1.val < list2.val) {
       tail.next = list1;
       list1 = list1.next!;
     } else {
       tail.next = list2;
-      list2 = list2.next;
+      list2 = list2.next!;
     }
     tail = tail.next!;
   }
@@ -73,20 +73,37 @@ function mergeKSorted1(lists: Array<ListNode | null>): number {
 
 function mergeKSorted2(lists: Array<ListNode | null>): number {
   let count = 0;
+  if (lists.length == 0) return 0;
+
+  while (lists.length > 1) {
+    const temp: Array<ListNode | null> = [];
+
+    for (let i = 0; i < lists.length; i += 2) {
+      const [temp1, temp2] = mergeTwo(
+        lists[i],
+        i + 1 < lists.length ? lists[i + 1] : null,
+      );
+      temp.push(temp1 as ListNode | null);
+      count += temp2 as number;
+    }
+
+    lists = temp;
+  }
 
   return count;
 }
 
 function main() {
-  const k = 5;
-  const n = 10;
+  const k = 1000;
+  const n = 100;
   const arr: number[][] = [...new Array(k)].map(() => {
     return [...new Array(n)].map((_, i) => i + 1);
   });
 
-  const lists: Array<ListNode | null> = arr.map(convertArrayToLL);
-  const method1 = mergeKSorted1(lists);
-  const method2 = mergeKSorted2(lists);
+  const lists1: Array<ListNode | null> = arr.map(convertArrayToLL);
+  const lists2: Array<ListNode | null> = arr.map(convertArrayToLL);
+  const method1 = mergeKSorted1(lists1);
+  const method2 = mergeKSorted2(lists2);
 
   console.log(`for k=${k} and n=${n}`);
   console.log(`method1, comparisons: ${method1}`);
