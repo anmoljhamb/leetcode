b81600f289562326fe7f46bc2cd8fbc7f3301174
Anmol Jhamb <talktoanmol@outlook.com>
2024-06-17 21:50:16 +0530

    028 first index

---
+++ 17thJune2024/028_first_index.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  int strStr(string haystack, string needle) {
+    int n = haystack.size();
+    int m = needle.size();
+    vector<int> lps = vector<int>(m);
+
+    int prev_lps = 0, i = 1, j = 0;
+
+    while (i < needle.size()) {
+      if (needle[i] == needle[i - 1]) {
+        lps[i] = prev_lps + 1;
+        prev_lps = lps[i];
+        i++;
+        continue;
+      }
+
+      if (prev_lps == 0) {
+        lps[i] = 0;
+        i++;
+      } else {
+        prev_lps = lps[prev_lps - 1];
+      }
+    }
+
+    i = 0;
+    j = 0;
+
+    while (i < n) {
+      if (haystack[i] == needle[j]) {
+        i++;
+        j++;
+      } else {
+        if (j == 0) {
+          i++;
+        } else {
+          j = lps[j - 1];
+        }
+      }
+
+      if (j == m)
+        return i - m;
+    }
+
+    return -1;
+  }
+};
